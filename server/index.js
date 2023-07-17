9c10f10d14fb1ea9ba3dba3b8df0e92da876b37d
Agu Chux <agu.chux@yahoo.com>
2023-07-17 22:38:26 +0100

    Vera: Cleanup

--- server/index.js
+++ server/index.js
@@ -1,32 +1,32 @@
 import express from "express";
 import cors from "cors";
-import morgan from "morgan";
-import connect from "./db/connec.js"
-import router from "./router/route.js";
-
-// Handles .end file read
 import * as dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
+
+// import morgan from "morgan";
+// import connect from "./db/connec.js"
+// import router from "./router/route.js";
+
+// Handles .end file read
+
+// app.use(morgan("tiny"));
 app.use(cors());
 app.disable('x-powered-by');
 
-const port = 8080
+const port = process.env.PORT || 8000;
 
 app.get("/", (req ,res)=>{
     res.status(201).json("Home GET Request")
 })
-app.use("/api", router )
 
-
+// app.use("/api", router )
 
 app.listen(port, ()=>{
     console.log(`Server conected to http://localhost:${port}`)
 })
-
 
 // Helps to start server only when there is a valid connection
 // connect().then(()=>{
